fa83c58ef52b8c0d63d22923610c6f53b97560cc
Kate Pond <kate@thepondsedge.com>
2018-05-29 15:26:23 -0700

    adds names to the title of the app

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Chatlog from './components/Chatlog';
 class App extends Component {
   render() {
     console.log(chatMessages);
-
+    
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Chat between {chatMessages[0].sender} and {chatMessages[1].sender}</h1>
         </header>
         <main className="App-main">
         <Chatlog chatData={chatMessages} />
